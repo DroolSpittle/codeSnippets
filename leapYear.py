e9a8c2909d036f71bea0a5dac071ba85d09bd4a8
DroolSpittle <46784778+DroolSpittle@users.noreply.github.com>
2021-12-02 12:15:31 -0500

    Create leapYear.py

---
+++ leapYear.py
@@ -0,0 +1,24 @@
+def is_leap(year):
+    leap = False
+    
+    if year % 4 != 0:
+        return leap
+    elif year % 4 == 0 and year % 100 != 0:
+        return True
+    elif year % 100 == 0 and year % 400 == 0:
+        return True
+    else:
+        return False
+    
+    
+    return leap
+
+
+
+
+##############
+##Better Way##
+##############
+
+def is_leap(year):
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
