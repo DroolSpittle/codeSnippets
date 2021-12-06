6f0595675df6355e057e2bd276f34e6d23d39b0f
DroolSpittle <46784778+DroolSpittle@users.noreply.github.com>
2021-12-06 09:29:36 -0500

    Create mutateString.py

---
+++ mutateString.py
@@ -0,0 +1,11 @@
+#take a string, then take a position and character to add, and add it to the string
+
+def mutate_string(string, position, character):
+    string = string [:position] + character + string[position + 1:]
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
